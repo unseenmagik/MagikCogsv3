5dc648cd5e717541393607cdabddcc99d04d64b9
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-01-04 13:38:42 +0000

    Update about.py
    changed embed colour

--- about/about.py
+++ about/about.py
@@ -9,7 +9,7 @@ class About:
     async def about(self, ctx):
         """About Magik Bot"""
         
-        embed=discord.Embed(title="About Magik Bot V3", url='http://www.magikbot.co.uk', description="The 3rd version of a discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
+        embed=discord.Embed(title="About Magik Bot V3", url='http://www.magikbot.co.uk', description="The 3rd version of a discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x545454)
         embed.set_author(name="Magik Bot V3", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/avatars/375541314139324416/054ca0e904c2120658a1d31623819703.png?size=1024')
         embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/375541314139324416/054ca0e904c2120658a1d31623819703.png?size=1024')
         embed.add_field(name="Discord Support", value="https://discord.gg/kQjTw5Z", inline=True)
