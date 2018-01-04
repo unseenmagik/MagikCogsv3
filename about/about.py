90ce6b429011902a3e71e3750799c70777d5092e
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-01-04 13:37:12 +0000

    Update about.py
    updated about information

--- about/about.py
+++ about/about.py
@@ -9,13 +9,13 @@ class About:
     async def about(self, ctx):
         """About Magik Bot"""
         
-        embed=discord.Embed(title="About Magik Bot", url='http://www.magikbot.co.uk', description="A discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
-        embed.set_author(name="Magik bot", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/avatars/375541314139324416/054ca0e904c2120658a1d31623819703.png?size=1024')
+        embed=discord.Embed(title="About Magik Bot V3", url='http://www.magikbot.co.uk', description="The 3rd version of a discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
+        embed.set_author(name="Magik Bot V3", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/avatars/375541314139324416/054ca0e904c2120658a1d31623819703.png?size=1024')
         embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/375541314139324416/054ca0e904c2120658a1d31623819703.png?size=1024')
         embed.add_field(name="Discord Support", value="https://discord.gg/kQjTw5Z", inline=True)
         embed.add_field(name="Website", value="http://www.magikbot.co.uk", inline=True)
         embed.add_field(name="Auther", value="Magik#0203", inline=False)
-        embed.add_field(name="Discord & API Version", value="Discord - 3.5.2 & API version 0.16.12", inline=True)
+        embed.add_field(name="Discord & API Version", value="Discord - 3.5.2 & API version 1.0.0a", inline=True)
         embed.set_footer(text="Magik Bot - Providing Discord support since September 2017")
         
         await ctx.send(embed=embed)
