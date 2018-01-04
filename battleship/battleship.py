2165d1dfb8066fd055ed7114d8b329ca8f53d42d
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-01-04 18:10:27 +0000

    Update battleship.py
    initial copy from V2

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -3,23 +3,20 @@ from discord.ext import commands
 from random import randint
 
 class Battleship:
-    
-    import discord
-from discord.ext import commands
-from random import randint
 
-class Battleship:
-
-    @commands.command(pass_context=True)
-    async def battlehelp(self, ctx):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def battlehelp(self):
 
         embed=discord.Embed(
             title="About Battleship", 
             description="~ A simple game of Battle Ships.\n:black_circle: = Open Target\n:red_circle: = Missed Target\n:large_blue_circle: = Target Hit\n⚪ = Location of ships (at the end of the game)", 
             color=0x207cee)
         embed.set_author(
-            name="Made by xDp64x and UnseenMagik",
-            icon_url='https://cdn.discordapp.com/attachments/342761826322481152/342892790935584769/dp_logo.png')
+            name="Magik Bot",
+            url = 'http://www.magikbot.co.uk',
+            icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
         embed.set_thumbnail(
             url='https://cdn.discordapp.com/attachments/355249562719617024/365100412874784768/Battleship-ubicom-VIDEO-launch_trailer_2016_08_02-712x712_Desktop_261122.png')
         embed.add_field(
@@ -42,25 +39,27 @@ class Battleship:
         embed.set_footer(
             text="Possible by - xDp64x and UnseenMagik")
         
-        instructions = await ctx.send(embed=embed) 
-
+        instructions = await self.bot.say(embed=embed) 
     @commands.command(pass_context=True)
     async def battleship(self, ctx):
 
         #----------------------------------------------------------------# 
         num = 0
         num2 = 0
-        num3 = 0
         msg = ""
         msg2 = ""
         total = 0
         turn = 10
         turn2 = 0
         check = 0
-        loop = True
         embedPrint = 0
+        var = 0
+        stop = 0
+        not2 = 0
+        colour = 0x0FFF00
+        fin = 0
+        
         error = "Error. Invalid Response."
-
         miss = "You missed my battleship!"
         hit1 = "You sunk part of a battleship!"
         hit2 = "You sunk a battleship!"
@@ -71,67 +70,11 @@ class Battleship:
         reply2 = ""
         reply = ""
         shipM = ""
-        shipP = ""
+
         board = []
         seperate = []
         author = ctx.message.author
         channel = ctx.message.channel
-
-
-
-        """embed=discord.Embed(
-            title="About Battleships", 
-            description="~ A simple game of Battle Ships built into Magik Bot.\n:black_circle: - Open Target\n:red_circle: - Missed Target\n:large_blue_circle: = Target Hit\n⚪ = Location of ships (at the end of the game)", 
-            color=0x207cee)
-        embed.set_author(
-            name="Battleship", url='http://www.magikbot.co.uk', 
-            icon_url='https://cdn.discordapp.com/attachments/355249562719617024/357107055691169797/MB_Icon.png')
-        embed.set_thumbnail(
-            url='https://cdn.discordapp.com/attachments/355249562719617024/365100412874784768/Battleship-ubicom-VIDEO-launch_trailer_2016_08_02-712x712_Desktop_261122.png')
-        embed.add_field(
-            name="How to play", 
-            value="Enter your X and Y value as a comment like this `4 2`. No prefix required. Type `cancel` to stop the game.", 
-            inline=True)
-        embed.add_field(
-            name="How many turns", 
-            value="You have 10 attempts to hit my 4 ships", 
-            inline=True)
-        embed.add_field(
-            name="Author", 
-            value="UnseenMagik & Dp", 
-            inline=True)
-        embed.add_field(
-            name="Battleship Board Layout",
-            value=("```Y   1 O O O O O\n"
-                   "    2 O O O O O\n"
-                   "A   3 O O O O O\n"
-                   "X   4 O O O O O\n"
-                   "I   5 O O O O O\n"
-                   "S   0 1 2 3 4 5\n"
-                   "    X   A X I S```"), inline=True)
-        embed.set_footer(
-            text="Magik Bot - Providing Discord support since September 2017")
-        
-        await ctx.send(embed=embed)""" 
-        
-        
-
-
-
-        """
-            10 9 8 7 6 5 4 3 2 1
-        Y    O O O O O O O O O O  10
-             O O O O O O O O O O  9
-        A    O O O O O O O O O O  8
-        X    O O O O O O O O O O  7
-        I    O O O O O O O O O O  6
-        s    O O O O O O O O O O  5
-             O O O O O O O O O O  4
-             O O O O O O O O O O  3
-             O O O O O O O O O O  2
-             O O O O O O O O O O  1
-            X Axis
-        """
 
         for x in range(5): #Size of the board
             board.append([":black_circle:"] * 5)
@@ -140,19 +83,9 @@ class Battleship:
             i = "\n"
             for x in board:
                 i = i + " ".join(x)+"\n"
-                #print(len(x))
+
             i += ""
-            return i
-        
-        
-
-
-
-        print ("Let's play Battleship!")
-        print (" ")
-        await ctx.send("Let's play Battleship!"+ "\n")
- 
-        
+            return i        
 
         def random_x(board):
             return randint(0, len(board) - 1)
@@ -198,15 +131,19 @@ class Battleship:
         l = len(board)
         
         #For debugging purposes
-        print("Ship 1: ", ship_x+1, ship_y+1)
-        print("Ship 2: ", ship1a+1, ship1b, " ", ship1d+1, ship1b)
-        print("Ship 3: ", ship2a+1, ship2b, " ", ship2a+1, ship2c)
+        #print("Ship 1: ", ship_x+1, ship_y+1)
+        #print("Ship 2: ", ship1a+1, ship1b, " ", ship1d+1, ship1b)
+        #print("Ship 3: ", ship2a+1, ship2b, " ", ship2a+1, ship2c)
 
-
-        def embed_board(turn2):
+        def embed_board(turn2, colour, author2):
             embed=discord.Embed(
                 title="The Board",
-                description=" ",)
+                description=" ",
+                color=colour)
+            embed.add_field(
+                name="Owner:",
+                value="{}'s Board".format(ctx.message.author.mention),
+                inline=True)
             embed.add_field(
                 name="Turn "+str(turn2),
                 value=print_board(board),
@@ -215,41 +152,39 @@ class Battleship:
             return embed
  
         while turn != 0:
-
-
-            #for turn in range(10):
+            author2 = ctx.message.author
+            not2 = 0
             reply2 = ""
-            """embed=discord.Embed(
-                title="The Board",
-                description=" ",)
-            embed.add_field(
-                name="Current Board ",
-                value=print_board(board),
-                inline=True)"""
-            #reply = embed
-            reply = embed_board(turn2)
+
+            reply = embed_board(turn2, colour, author2)
             check += 1
             print("Turns:"+str(turn))
 
-            #Send Embed here, edit later#
+            #Send Embed here, edit later
             if embedPrint == 0:
-                message_Embed = await ctx.send(embed=reply)
+                message_Embed = await self.bot.say(embed=reply)
 
             else:
-                await ctx.edit_message(message_Embed, embed=reply)
+                await self.bot.edit_message(message_Embed, embed=reply)
 
             guess_x = -1
             guess_y = -1
-            #await ctx.send_typing(channel)
-            guessing = await ctx.send("\n"+"Guess X and Y:")                
+            await self.bot.send_typing(channel)
+            guessing = await self.bot.say("\n"+"Guess X and Y:")                
             msg = await self.bot.wait_for_message(timeout=30,author=author, channel=channel)
-            await ctx.delete_message(guessing)
+
+            if not msg:
+                var += 1
+                break
+                
+            await self.bot.delete_message(guessing)
+            #Gets the no message, when times out. Working?
+            
 
             if msg.content == "Cancel" or msg.content == "cancel":
-                await ctx.send("Stopping game.")
-                print("Stopping the game.")
-                #loop = False
-                break
+                    print("Stopping the game.")
+                    stop += 1
+                    break
             #Catches any errors, such as bad input. Not numbers, not 2 answers, etc.
             try:
                 msg2 = msg.content
@@ -261,6 +196,7 @@ class Battleship:
             except IndexError:
                 reply2 = error
                 print("Invalid Response. IndexError")
+                not2 += 1
                 turn += 1
                 turn2 -= 1
                     
@@ -268,12 +204,14 @@ class Battleship:
                 turn += 1
                 turn2 -= 1
                 reply2 = error
+                not2 += 1
                 print("Invalid Response. ValueError")
                     
             except UnboundLocalError:
                 turn += 1
                 turn2 -= 1
                 reply2 = error
+                not2 += 1
                 print("Invalid. UnboundLocalError")
                 
             #Deletes users answer, like 1 3. Needs proper perms though.   
@@ -282,141 +220,175 @@ class Battleship:
                     
             except discord.errors.Forbidden:
                 print('discord.errors.Forbidden')
-                await ctx.send('Error. Don\'t have the permissions. Stopping game.')
-                #loop = False
+                await self.bot.say('Error. Don\'t have the permissions. Stopping game.')
                 break
                 
             if total == 4:
-                await ctx.send("You sunk all the ships!")
+                await self.bot.say("You sunk all the ships!")
                 print("All ships sunk.")
                     
                 if total == 4:
-                    await ctx.send("You hit em all captain.\n Game Over.")
-                    await ctx.edit_message(message_Embed, embed=reply)
-                    #loop = False
-                        
+                    board[guess_x][guess_y] = ":large_blue_circle:"
+                    colour = 0xCF9C00
+                    reply2 = "You hit em all captain.\n Game Over."
+                    await self.bot.delete_message(message_Embed)
+                    fin += 1
+                    
+                await self.bot.delete_message(ShipM)        
                 break
                 
             elif guess_x == ship_x and guess_y == ship_y:
-                board[guess_x][guess_y] = ":large_blue_circle:"
-                print("Sunk a ship.")
-                reply2 = hit2
-                total += 1
+                #Experimental
+                if board[guess_x][guess_y] == ":large_blue_circle:":
+                    turn += 1
+                    turn2 -= 1
+                else:
+                    colour = 0x55acee
+                    board[guess_x][guess_y] = ":large_blue_circle:"
+                    print("Sunk a ship.")
+                    reply2 = hit2
+                    total += 1
 
             #-------------------------------------------#    
             elif guess_x == ship1a and guess_y == ship1b:
-                board[guess_x][guess_y] = ":large_blue_circle:"
-                if num == 0:
-                    print("Part of ship sunk.")
-                    reply2 = hit1
-                    num += 1
-
+                if board[guess_x][guess_y] == ":large_blue_circle:":
+                    turn += 1
+                    turn2 -= 1
                 else:
-                    print("You sunk a battleship.")
-                    reply2 = hit2
+                    colour = 0x55acee
+                    board[guess_x][guess_y] = ":large_blue_circle:"
+                    if num == 0:
+                        reply2 = hit1
+                        num += 1
+
+                    else:
+                        reply2 = hit2
                         
-                total += 1 
+                    total += 1 
 
             elif guess_x == ship1d and guess_y == ship1b:
-
-                board[guess_x][guess_y] = ":large_blue_circle:"
-
-                if num == 0:
-                    print("You sunk part of a battleship!")
-                    reply2 = hit1
-                    num += 1
-
+                if board[guess_x][guess_y] == ":large_blue_circle:":
+                    turn += 1
+                    turn2 -= 1
                 else:
-                    print("You sunk a battleship.")
-                    reply2 = hit2
+                    colour = 0x55acee
+                    board[guess_x][guess_y] = ":large_blue_circle:"
+
+                    if num == 0:
+                        reply2 = hit1
+                        num += 1
+
+                    else:
+                        reply2 = hit2
                         
-                total += 1
+                    total += 1
                 #-----------------------------------------------#
             elif guess_x == ship2a and guess_y == ship2b:
-
-                board[guess_x][guess_y] = ":large_blue_circle:"
-
-                if num2 == 0:
-                    reply2 = hit1
-                    print("You sunk part of a battleship!")
-                    num2 += 1
-
+                if board[guess_x][guess_y] == ":large_blue_circle:":
+                    turn += 1
+                    turn2 -= 1
                 else:
-                    reply2 = hit2
-                    print("You sunk a battleship.")
+                    colour = 0x55acee
+                    board[guess_x][guess_y] = ":large_blue_circle:"
+
+                    if num2 == 0:
+                        reply2 = hit1
+                        num2 += 1
+
+                    else:
+                        reply2 = hit2
         
-                total += 1
+                    total += 1
 
             elif guess_x == ship2a and guess_y == ship2c:
-
-                board[guess_x][guess_y] = ":large_blue_circle:"
-
-                if num2 == 0:
-                    reply2 = hit1
-                    print("You sunk part of a battleship!")
-                    num2 += 1
-
-                else:
-                    reply2 = hit2
-                    print("You sunk a battleship.")
-
-                total += 1
-                #--------------------------------------#
-                
-
-
-            else:
-                if (guess_x < 0 or guess_x > l-1) or (guess_y < 0 or guess_y > l-1):
-                    reply2 = ocean
-                    print ("Oops, that's not even in the ocean.")
-                        
-                elif(board[guess_x][guess_y] == ":red_circle:"):
-                    reply2 = guess
+                if board[guess_x][guess_y] == ":large_blue_circle:":
                     turn += 1
-                    print ("You guessed that one already.")
-                        
+                    turn2 -= 1
                 else:
-                    print ("You missed my battleship!")
-                    board[guess_x][guess_y] = ":red_circle:"
-                    reply2 = miss
+                    colour = 0x55acee
+                    board[guess_x][guess_y] = ":large_blue_circle:"
 
-                    """if turn <= 0:
-                        reply2 = over
-                        print ("Game Over")
-                            
-                        board[ship_x][ship_y] = ":white_circle:"
-                        board[ship1d][ship1b] = ":white_circle:" 
-                        board[ship1a][ship1b] = ":white_circle:"
-                        board[ship2a][ship2b] = ":white_circle:"
-                        board[ship2a][ship2c] = ":white_circle:"
-                            
-                        await self.bot.edit_message(message_Embed, embed=reply)
-                        print(" ")
-                        print("Here are all the ships, they're labeled M.")
-                        break
-                        #loop = False"""         
+                    if num2 == 0:
+                        reply2 = hit1
+                        num2 += 1
+
+                    else:
+                        reply2 = hit2
+
+                    total += 1
+                #--------------------------------------#                
+            else:
+                if not2 >= 1:
+                    colour = 0x010000
+                    reply2 = "Error. Invalid Format."
+                    
+                elif not2 == 0:
+                    if (guess_x < 0 or guess_x > l-1) or (guess_y < 0 or guess_y > l-1):
+                        colour = 0x010000
+                        reply2 = ocean
+                        turn += 1
+                        turn2 -= 1
+                        
+                    elif(board[guess_x][guess_y] == ":red_circle:"):
+                        colour = 0x010000
+                        reply2 = guess
+                        turn += 1
+                        turn2 -= 1
+                        
+                    else:
+                        colour = 0xFF0000
+                        board[guess_x][guess_y] = ":red_circle:"
+                        reply2 = miss        
 
             if embedPrint == 0:
-                shipM = await ctx.send(reply2)
+                shipM = await self.bot.say(reply2)
             else:
-                await ctx.edit_message(shipM, reply2)
+                await self.bot.edit_message(shipM, reply2)
             embedPrint += 1
             turn -= 1
             turn2 += 1
+            
+            if turn2 == 10:
+                await self.bot.delete_message(shipM)
 
-        #reply2 = over
-        print ("Game Over")
+        if turn == 0:
+            colour = 0xFFFFFF
+            
+
                             
-        board[ship_x][ship_y] = ":white_circle:"
-        board[ship1d][ship1b] = ":white_circle:" 
-        board[ship1a][ship1b] = ":white_circle:"
-        board[ship2a][ship2b] = ":white_circle:"
-        board[ship2a][ship2c] = ":white_circle:"
-        reply = embed_board(turn2)                    
-        await ctx.edit_message(message_Embed, embed=reply)
-        await ctx.send(over)
-        print(" ")
-        print("Here are all the ships, they're labeled M.")
+            board[ship_x][ship_y] = ":white_circle:"
+            board[ship1d][ship1b] = ":white_circle:" 
+            board[ship1a][ship1b] = ":white_circle:"
+            board[ship2a][ship2b] = ":white_circle:"
+            board[ship2a][ship2c] = ":white_circle:"
+            reply = embed_board(turn2, colour, author)
+            print("Shows ships. Games over.")
+            await self.bot.edit_message(message_Embed, embed=reply)
+
+            reply2 = over
+
+        else:
+            if var == 1:
+                #await self.bot.delete_message(play)
+                await self.bot.delete_message(guessing)
+                await self.bot.delete_message(message_Embed)
+                
+                reply2 = "I got no response from you :cry: . Stopping game. "
+            elif stop == 1:
+                #await self.bot.delete_message(play)
+                await self.bot.delete_message(message_Embed)
+                await self.bot.delete_message(msg)
+                
+                reply2 = "The game has been cancelled."
+            elif fin == 1:
+                turn2 = 10
+                colour = 0xCF9C00
+                reply = embed_board(turn2, colour, author2)
+                await self.bot.say(embed=reply)
+        await self.bot.say(reply2)
+           
 
 
             #----------------------------------------------------------------#
+
+
