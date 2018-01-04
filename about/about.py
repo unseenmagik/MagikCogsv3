2976f6f4c99025be9758d9ae00198df9987d2da9
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-01-04 12:46:40 +0000

    Update about.py

--- about/about.py
+++ about/about.py
@@ -1,15 +1,21 @@
 from discord.ext import commands
 
-class About:
+class Magikbot:
     """More information about Magik Bot"""
     
-    @commands.command()
-    @checks.admin_or_permissions(ban_members=True)
-    async def ban(self, ctx, user: discord.Member, reason: str=None):
-        await ctx.guild.ban(user)
-        case = modlog.create_case(
-            ctx.guild, ctx.message.created_at, "ban", user,
-            ctx.author, reason, until=None, channel=None
-        )
-        await ctx.send("Done. It was about time.")
+
+    @commands.command(pass_context=True)
+    async def magikbot(self):
+        """About Magik Bot"""
+        
+        embed=discord.Embed(title="About Magik Bot", url='http://www.magikbot.co.uk', description="A discord bot made with love. Created for Discord Administration support. With hundreds of commands, admin, mod, support, games, fun and more. For more information see below:", color=0x207cee)
+        embed.set_author(name="Magik bot", url='http://www.magikbot.co.uk', icon_url='https://cdn.discordapp.com/avatars/375541314139324416/054ca0e904c2120658a1d31623819703.png?size=1024')
+        embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/375541314139324416/054ca0e904c2120658a1d31623819703.png?size=1024')
+        embed.add_field(name="Discord Support", value="https://discord.gg/kQjTw5Z", inline=True)
+        embed.add_field(name="Website", value="http://www.magikbot.co.uk", inline=True)
+        embed.add_field(name="Auther", value="Magik#0203", inline=False)
+        embed.add_field(name="Discord & API Version", value="Discord - 3.5.2 & API version 0.16.12", inline=True)
+        embed.set_footer(text="Magik Bot - Providing Discord support since September 2017")
+        
+        await self.bot.say(embed=embed)
        
