b8b449af7399a69a1b39b1fc308c615ca68a0468
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-01-09 08:58:29 +0000

    Update say.py

--- say/say.py
+++ say/say.py
@@ -10,9 +10,3 @@ class Say:
     async def send(self, ctx): # Had to choose something else than say :c have a better idea ?
         await ctx.send(ctx.message.channel, page)
 
-    @send.command(pass_context=True)
-    async def here(self, ctx, *, text):
-        """Say a message in the actual channel"""
-        
-        message = ctx.message
-        server = message.guild
