1d31acd6380c13a84dd79afc2d781c446ba2f650
xDp64xG <brow.paul64@gmail.com>
2018-01-09 17:15:04 -0500

    Update say.py
    A successful say command for v3 with auto deletion.

--- say/say.py
+++ say/say.py
@@ -8,30 +8,40 @@ class Say:
     @commands.command(pass_context=True)
     async def send(self, ctx): 
         """This does stuff!"""
+        msg = ctx.message.content
+        message = ctx.message
+        server = message.guild
+        channel = ctx.channel
+
+        msg = msg[6:]
+
+        await message.delete()
+        
+        await ctx.send(msg)
         # Your code will go here
         #Will focus on here later on, should manually input the help than using self.
-        var = here(self, ctx)
+        
         """if ctx.invoked_subcommand is None
             pages = self.bot.formatter.format_help_for(ctx, ctx.command)
         for page in pages:
             await ctx.send(ctx.message.channel, page)"""
            
         
-    @send.command(pass_context=True)
+    """@send.command(pass_context=True)
     async def here(self, ctx, *, text):
-        """Say a message in the actual channel"""
+        Say a message in the actual channel
         
         message = ctx.message
         server = message.guild
         #Remove this because we would always want your message to auto delete
-        """if server.id not in self.settings:
+        if server.id not in self.settings:
             self.settings[server.id] = {'autodelete': '0'}
 
         if self.settings[server.id]['autodelete'] == '1':
             await self.bot.delete_message(message)
 
         else:
-            pass"""
+            pass
         try:
             await self.bot.delete_message(message)
             
@@ -43,27 +53,27 @@ class Say:
             
     @send.command(pass_context=True)
     async def channel(self, ctx, channel : discord.Channel, *, text):
-        """Say a message in the chosen channel"""
+        Say a message in the chosen channel
 
         message = ctx.message
         server = message.guild
         
         #Same thing as above
-        """if server.id not in self.settings:
+        if server.id not in self.settings:
             self.settings[server.id] = {'autodelete': '0'}
 
         if self.settings[server.id]['autodelete'] == '0' or server.id not in self.settings:
             pass
         
         else:
-            await self.bot.delete_message(message)"""
+            await self.bot.delete_message(message)
         try:
             await self.bot.delete_message(message)
             
         except Forbidden:
             print("Unable to delete Users message. Proceeding...")
             
-        await ctx.send(message)
+        await ctx.send(message)"""
 
 
 
