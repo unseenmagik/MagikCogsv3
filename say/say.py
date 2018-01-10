5ce272ee857850a47e6c89f953f63346206b2fa6
xDp64xG <brow.paul64@gmail.com>
2018-01-10 09:17:24 -0500

    Update say.py
    Fixed comment for better explanation if anyone tries to understand the code.

--- say/say.py
+++ say/say.py
@@ -2,12 +2,12 @@ import discord
 from discord.ext import commands
 
 class Say:
-    """Make your bot say something in the channel you want."""
-
+    
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_server=True)
     async def say(self, ctx): 
-        """Make your bot say something in the channel you want.."""
+        """Make your bot say something in the channel you use the command in."""
+
         msg = ctx.message.content
         message = ctx.message
         
