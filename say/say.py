8239d69a65325f8abc78df435a9ab82828cdff1d
xDp64xG <brow.paul64@gmail.com>
2018-01-10 09:06:52 -0500

    Update say.py
    Only admins are able to do the command, and changed the command to say, "[p]say <text>".

--- say/say.py
+++ say/say.py
@@ -4,17 +4,15 @@ from discord.ext import commands
 class Say:
     """Make your bot say something in the channel you want."""
 
-
     @commands.command(pass_context=True)
-    async def send(self, ctx): 
+    @checks.admin_or_permissions(manage_server=True)
+    async def say(self, ctx): 
         """Make your bot say something in the channel you want.."""
         msg = ctx.message.content
         message = ctx.message
-        server = message.guild
-        channel = ctx.channel
         
-        #This gets rid of the command "[p]send"
-        msg = msg[6:]
+        #This gets rid of the command "[p]say"
+        msg = msg[5:]
 
         await message.delete()
         await ctx.send(msg)
