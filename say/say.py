50849fc23fe1a1442be486e64a12a8d5efedbaf7
xDp64xG <brow.paul64@gmail.com>
2018-01-10 22:24:19 -0500

    Extreme updates
    To start...I added in a try and except for having the lack of perms of the bot deleting your message.
    Another addition is a try and except for an empty command([p]say )
    Both of these errors return a message to the user that initiated the command and let's them know what happened. In a embed for a clean look.
    Only players who have admin permissions can use this command, because people would abuse this power.

--- say/say.py
+++ say/say.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+from redbot.core import checks
 
 class Say:
+
     
     @commands.command(pass_context=True)
-    @checks.admin_or_permissions(manage_server=True)
+    #@checks.admin_or_permissions(manage_server=True)
+    @checks.admin_or_permissions(administrator=True)
     async def say(self, ctx): 
         """Make your bot say something in the channel you use the command in."""
 
@@ -19,6 +22,26 @@ class Say:
             
         except discord.Forbidden:
             print("No permissions.")
-            Msg = "Unable to delete the command message, I don't have the adminstrative permissions to do so."
-            await ctx.send(author, Msg)
-        await ctx.send(msg)
+
+            def error(self, ctx):
+                embed=discord.Embed(
+                    title="Error:",
+                    description="Unable to delete the command message, I don't have the adminstrative permissions to do so.",
+                    color=0x207cee)
+                return embed
+
+            def error2(self, ctx):
+                embed=discord.Embed(
+                    title="Error:",
+                    description="Unable to send text. Its empty. :upside_down: ",
+                    color=0x207cee)
+                return embed
+
+            Msg = error(self, ctx)
+            #Msg = "Unable to delete the command message, I don't have the adminstrative permissions to do so."
+            await author.send(embed=Msg)
+        try:
+            await ctx.send(msg)
+        except discord.errors.HTTPException:
+            Msg2 = error2(self, ctx)
+            await author.send(embed=Msg2)
