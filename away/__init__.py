c57dfc3ca8d17846ff715778ccd70b0fa328cd8c
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-01-13 21:46:33 +0000

    Create __init__.py
    initial copy from Beta repo

---
+++ away/__init__.py
@@ -0,0 +1,22 @@
+from .away import Away
+from redbot.cogs.away.dataIO import dataIO
+import os
+
+def check_folder():
+    if not os.path.exists('data/away'):
+        print('Creating data/away folder...')
+        os.makedirs('data/away')
+
+
+def check_file():
+    f = 'data/away/away.json'
+    dataIO.save_json(f, {})
+    print('Creating default away.json...')
+
+
+def setup(bot):
+    check_folder()
+    check_file()
+    n = Away(bot)
+    bot.add_listener(n.listener, 'on_message')
+    bot.add_cog(n)
