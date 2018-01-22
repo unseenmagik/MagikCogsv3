bce599de7f389afc509ffc0748e14de9401d9d56
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-01-22 08:27:01 +0000

    Update pingtime.py

--- pingtime/pingtime.py
+++ pingtime/pingtime.py
@@ -3,21 +3,18 @@ from discord.ext import commands
 import datetime
 import time
 from random import choice, randint
+
 class Pingtime:
     """Ping, with time"""
-
 
     @commands.command(pass_context=True)
     async def pingtime(self, ctx):
 
-        """Pong."""
+        """Ping = Pong including ping time."""
 
         await ctx.send ('__*`Pinging...`*__')
         t1 = time.perf_counter()
 
-
-        
-        
         color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         color = int(color, 16)
         t2 = time.perf_counter()
@@ -27,7 +24,5 @@ class Pingtime:
         t3 = t3[2:4]
         #thedata = ("**Pong.**\nMessage Latency: " + t3 + "ms")
         data = discord.Embed(description='**Pong.**\nMessage Latency: {}ms'.format(t3), colour=discord.Colour(value=color))
-        
-
-    
+            
         await ctx.send(embed=data)
