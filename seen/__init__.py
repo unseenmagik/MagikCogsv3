6b448a050e42be4ba97a57ef24b8d06aa959f9d8
Magik#0203 <magik@magikbot.co.uk>
2018-01-28 21:04:41 +0000

    push from Beta for Public

---
+++ seen/__init__.py
@@ -0,0 +1,28 @@
+
+from .dataIO import dataIO
+import os
+import asyncio
+from .seen import Seen 
+
+"""def __init__(self, bot):
+    self.bot = bot
+    self.seen = dataIO.load_json('data/seen/seen.json')
+    self.new_data = False"""
+
+def check_folder():
+    if not os.path.exists('data/seen'):
+        print('Creating data/seen folder...')
+        os.makedirs('data/seen')
+
+def check_file():
+    f = 'data/seen/seen.json'
+    dataIO.save_json(f, {})
+    print('Creating default seen.json...')
+
+def setup(bot):
+    check_folder()
+    check_file()
+    n = Seen(bot)
+    loop = asyncio.get_event_loop()
+    loop.create_task(n.data_writer())
+    bot.add_cog(n)
