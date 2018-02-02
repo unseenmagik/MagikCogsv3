ddb311afb2d2cde5951e53ef1cc368ffad498c73
xDp64xG <brow.paul64@gmail.com>
2018-02-02 00:10:58 -0500

    Updated away
    From some testing, you can now have your json file not overwrite when the bot restarts

--- away/__init__.py
+++ away/__init__.py
@@ -1,6 +1,13 @@
 from .away import Away
 from .dataIO import dataIO
 import os
+from redbot.core.json_io import JsonIO
+from pathlib import Path
+
+dir = os.getcwd()
+config_dir = Path(dir)
+config_dir.mkdir(parents=True, exist_ok=True)
+g = config_dir / 'data/away/away.json'
 
 def check_folder():
     if not os.path.exists('data/away'):
@@ -9,14 +16,24 @@ def check_folder():
 
 
 def check_file():
-    f = 'data/away/away.json'
-    dataIO.save_json(f, {})
+    if not g.exists():
+        print('Test1')
+        print('{}')
+        return {}
+    return JsonIO(g)._load_json()
+    print('Test2')
     print('Creating default away.json...')
+def save_file():
+    config = check_file()
+    JsonIO(g)._save_json(config)
 
-
+def load_file():
+    JsonIO(g)._load_json()
+    print('Loaded')
 def setup(bot):
     check_folder()
-    check_file()
+    save_file()
+    load_file()
     n = Away(bot)
     bot.add_listener(n.listener, 'on_message')
     bot.add_cog(n)
--- away/away.py
+++ away/away.py
@@ -1,49 +1,60 @@
 import os
 import discord
-from .dataIO import dataIO
-from redbot.core import checks
+from redbot.core.json_io import JsonIO
 from discord.ext import commands
+from pathlib import Path
 
 class Away:
     def __init__(self, bot):
-        self.data = dataIO.load_json('data/away/away.json')
+        dir = os.getcwd()
+        config_dir = Path(dir)
+        config_dir.mkdir(parents=True, exist_ok=True)
+        g = config_dir / 'data/away/away.json'
+        self.data = JsonIO(g)._load_json()
     """Le away cog"""
 
     async def listener(self, message):
         tmp = {}
         server = message.guild
         channel = message.channel
-        if server.id not in self.data:
-            for mention in message.mentions:
-                tmp[mention] = True
-            for author in tmp:
-                if author.id in self.data:
-                    try:
-                        avatar = author.avatar_url if author.avatar else author.default_avatar_url
-                        if self.data[author.id]['MESSAGE']:
-                            em = discord.Embed(description=self.data[author.id]['MESSAGE'], color=discord.Color.blue())
-                            em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
-                        else:
-                            em = discord.Embed(color=discord.Color.blue())
-                            em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
-                        await channel.send(embed=em)
-                    except:
-                        if self.data[author.id]['MESSAGE']:
-                            msg = '{} is currently away and has set the following message: `{}`'.format(author.display_name, self.data[author.id]['MESSAGE'])
-                        else:
-                            msg = '{} is currently away'.format(author.display_name)
-                        await channel.send(msg)
+        #if server.id not in self.data:
+        for mention in message.mentions:
+            tmp[mention] = True
+        for author in tmp:
+            auth = str(author.id)
+            if str(author.id) in self.data or author.id in self.data:
+                avatar = author.avatar_url if author.avatar else author.default_avatar_url
+                try:
+                    em = discord.Embed(description=self.data[author.id]['MESSAGE'], color=discord.Color.blue())
+                except:
+                    em = discord.Embed(description=str(self.data[auth]['MESSAGE']), color=discord.Color.blue())
+                em.set_author(name='{} is currently away'.format(author.display_name), icon_url=avatar)
+                await channel.send(embed=em)
 
     @commands.command(pass_context=True, name="away")
     async def _away(self, context, *message: str):
+        dir = os.getcwd()
+        config_dir = Path(dir)
+        config_dir.mkdir(parents=True, exist_ok=True)
+        g = config_dir / 'data/away/away.json'
         """Tell the bot you're away or back."""
         author = context.message.author
-        if author.id in self.data:
-            del self.data[author.id]
-            msg = 'You\'re now back.'
+        print(self.data)
+        auth = str(author.id)
+        ctxx = int(author.id)
+        if author.id in self.data or str(author.id) in self.data:
+            try:
+                del self.data[author.id]
+            except:
+                del self.data[auth]
+            msg = "You're now back."
         else:
-            self.data[context.message.author.id] = {}
-            self.data[context.message.author.id]['MESSAGE'] = ' '.join(context.message.clean_content.split()[1:])
-            msg = 'You\'re now set as away.'
-        dataIO.save_json('data/away/away.json', self.data)
+            try:
+                self.data[context.message.author.id] = {}
+                self.data[context.message.author.id]['MESSAGE'] = ' '.join(context.message.clean_content.split()[1:])
+            except:
+                self.data[ctxx] = {}
+                self.data[ctxx]['MESSAGE'] = ' '.join(context.message.clean_content.split()[1:])
+            msg = "You're now set as away."
+        JsonIO(g)._save_json(self.data)
         await context.send(msg)
