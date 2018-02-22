32853bf813339af48673294ccc7fc8f3bfec1144
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-02-22 08:07:48 +0000

    Update distance.py
    Updated cooldown times.

--- distance/distance.py
+++ distance/distance.py
@@ -52,88 +52,148 @@ class Distance:
 
             
             if dist >= 1500:
-                time = "120 min"
+                time = "120 minutes"
 
-            elif dist >= 1000 and dist <= 1500:
-                time = "90 min"
+            elif dist >= 1403 and dist <= 1500:
+                time = "120 minutes"
 
-            elif dist >= 820 and dist <= 1000:
-                time = "85 min"
+            elif dist >= 1344 and dist <= 1403:
+                time = "119 minutes"
 
-            elif dist >= 720 and dist <= 820:
-                time = "80 min"
+            elif dist >= 1300 and dist <= 1344:
+                time = "117 minutes"
 
-            elif dist >= 700 and dist <= 720:
-                time = "75 min"
+            elif dist >= 1221 and dist <= 1300:
+                time = "112 minutes"
 
-            elif dist >= 565 and dist <= 700:
-                time = "67 min"
+            elif dist >= 1180 and dist <= 1221:
+                time = "109 minutes"
 
-            elif dist >= 500 and dist <= 565:
-                time = "60 min"
+            elif dist >= 1020 and dist <= 1180:
+                time = "101 minutes"
 
-            elif dist >= 460 and dist <= 500:
-                time = "58 min"
+            elif dist >= 1007 and dist <= 1020:
+                time = "97 minutes"
 
-            elif dist >= 345 and dist <= 460:
-                time = "50 min"
+            elif dist >= 948 and dist <= 1007:
+                time = "94 minutes"
 
-            elif dist >= 250 and dist <= 345:
-                time = "45 min"
+            elif dist >= 897 and dist <= 948:
+                time = "90 minutes"
 
-            elif dist >= 220 and dist <= 250:
-                time = "40 min"
+            elif dist >= 839 and dist <= 897:
+                time = "88 minutes"
 
-            elif dist >= 100 and dist <= 220:
-                time = "35 min"
+            elif dist >= 802 and dist <= 839:
+                time = "83 minutes"
 
-            elif dist >= 80 and dist <= 100:
-                time = "27 min"
+            elif dist >= 751 and dist <= 802:
+                time = "81 minutes"
 
-            elif dist >= 75 and dist <= 80:
-                time = "25 min"
+            elif dist >= 700 and dist <= 751:
+                time = "76 minutes"
 
-            elif dist >= 65 and dist <= 75:
-                time = "22 min"
+            elif dist >= 650 and dist <= 700:
+                time = "73 minutes"
 
-            elif dist >= 40 and dist <= 65:
-                time = "19 min"
+            elif dist >= 600 and dist <= 650:
+                time = "69 minutes"
 
+            elif dist >= 550 and dist <= 600:
+                time = "65 minutes"
+
+            elif dist >= 500 and dist <= 550:
+                time = "61 minutes"
+
+            elif dist >= 450 and dist <= 500:
+                time = "58 minutes"
+
+            elif dist >= 400 and dist <= 450:
+                time = "54 minutes"
+
+            elif dist >= 350 and dist <= 400:
+                time = "49 minutes"
+
+            elif dist >= 328 and dist <= 350:
+                time = "48 minutes"
+
+            elif dist >= 300 and dist <= 328:
+                time = "46 minutes"
+
+            elif dist >= 250 and dist <= 300:
+                time = "41 minutes"
+
+            elif dist >= 201 and dist <= 250:
+                time = "36 minutes"
+
+            elif dist >= 175 and dist <= 201:
+                time = "33 minutes"
+
+            elif dist >= 150 and dist <= 175:
+                time = "31 minutes"
+                
+            elif dist >= 125 and dist <= 150:
+                time = "28 minutes"
+                
+            elif dist >= 100 and dist <= 125:
+                time = "26 minutes"
+
+            elif dist >= 90 and dist <= 100:
+                time = "24 minutes"
+                
+            elif dist >= 80 and dist <= 90:
+                time = "23 minutes"
+                
+            elif dist >= 70 and dist <= 80:
+                time = "22 minutes"
+                
+            elif dist >= 60 and dist <= 70:
+                time = "21 minutes"
+                
+            elif dist >= 50 and dist <= 60:
+                time = "20 minutes"
+                
+            elif dist >= 45 and dist <= 50:
+                time = "19 minutes" 
+                
+            elif dist >= 40 and dist <= 45:
+                time = "18 minutes"
+                
             elif dist >= 35 and dist <= 40:
-                time = "17 min"
-
+                time = "17 minutes"
+                
             elif dist >= 30 and dist <= 35:
-                time = "15 min"
-
-            elif dist >= 20 and dist <= 30:
-                time = "10 min"
-
+                time = "16 minutes"
+                
+            elif dist >= 25 and dist <= 30:
+                time = "14 minutes"
+                
+            elif dist >= 20 and dist <= 25:
+                time = "11 minutes"
+                
             elif dist >= 15 and dist <= 20:
-                time = "8 min"
-
+                time = "8 minutes"
+                
             elif dist >= 10 and dist <= 15:
-                time = "7 min"
-
+                time = "6 minutes" 
+                
             elif dist >= 8 and dist <= 10:
-                time = "6 min"
-
+                time = "4 minutes"
+                
             elif dist >= 5 and dist <= 8:
-                time = "2 min 30 sec"
-
+                time = "3 minutes"
+                
             elif dist >= 4 and dist <= 5:
-                time = "2 min"
-
+                time = "2 minutes"   
+                
             elif dist >= 3 and dist <= 4:
-                time = "2 min"
-
+                time = "2 minutes"
+                
             elif dist >= 2 and dist <= 3:
-                time = "1 min 30 sec"
-
-            elif dist >= 1 and dist <= 2:
-                time = "1 min 30 sec"
+                time = "1 minutes"                
 
             elif dist and dist <= 1:
-                time = "1 min"
+                time = "48 seconds"
                       
             return time
 
