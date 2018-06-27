215ebfc34fa33431b62ba863af8f61ba3e030b87
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-06-27 19:43:57 +0100

    Create avatar.py
    initial v3 release

---
+++ avatar/avatar.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+
+class Avatar:
+    """View a users avatar"""
+
+    @commands.command(pass_context=True)
+    async def avatar(self, ctx, member:discord.Member=None):
+        async with ctx.channel.typing():
+            if member is None:
+                member = ctx.message.author
+            if member.is_avatar_animated():
+                async with self.session.get(member.avatar_url_as(format="gif")) as resp:
+                    data = await resp.read()
+                file = discord.File(io.BytesIO(data),filename="{}.gif".format(member.name))
+            if not member.is_avatar_animated():
+                async with self.session.get(member.avatar_url_as(static_format="png")) as resp:
+                    data = await resp.read()
+                file = discord.File(io.BytesIO(data),filename="{}.png".format(member.name))
+            await ctx.send(file=file)
