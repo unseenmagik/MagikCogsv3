398b6a137ce912635d3f4ccb76654902c57c90e7
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2018-06-27 20:52:18 +0100

    Update __init__.py

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -1,7 +1,4 @@
 from .avatar import Avatar
-from redbot.core import data_manager
 
 def setup(bot):
-    n = Avatar(bot)
-    data_manager.load_bundled_data(n, __file__)
-    bot.add_cog(n)
+    bot.add_cog(Avatar(bot))
