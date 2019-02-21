7cd153c1053a91f1f79dedbe58891a13330c49dd
UnseenMagik <30931287+unseenmagik@users.noreply.github.com>
2019-02-21 15:41:50 +0000

    Update members.py

--- members/members.py
+++ members/members.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from redbot.core import checks
-from redbot.core import commads
+from redbot.core import commads.Cog
 
 class Members (commands.Cog):
     def __init__(self, bot):
